b96b6e44f46a3bfa90c2fd65383ef6d51160b190
Miga <myagmarnaran.n@nomadicss.mn>
2024-04-16 19:09:57 +0800

    static files

--- ashop/urls.py
+++ ashop/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from home import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
